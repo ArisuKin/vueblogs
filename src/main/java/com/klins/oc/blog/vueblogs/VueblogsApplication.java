fdaf89d7fa8ee8fadf0f6bd80cfd668519baa09b
KCDev <10140540+klincode@user.noreply.gitee.com>
2022-06-17 23:00:59 +0800

    up one

---
+++ src/main/java/com/klins/oc/blog/vueblogs/VueblogsApplication.java
@@ -0,0 +1,13 @@
+package com.klins.oc.blog.vueblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueblogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueblogsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klins/oc/blog/vueblogs/VueblogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klins.oc.blog.vueblogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueblogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
