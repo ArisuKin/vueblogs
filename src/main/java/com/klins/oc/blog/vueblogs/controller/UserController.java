e2aee1c64110e22f174b3202e96cfead9eb09a91
KCDev <10140540+klincode@user.noreply.gitee.com>
2022-06-20 17:02:52 +0800

    up one

--- src/main/java/com/klins/oc/blog/vueblogs/controller/UserController.java
+++ src/main/java/com/klins/oc/blog/vueblogs/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.klins.oc.blog.vueblogs.controller;
 
 
+import com.klins.oc.blog.vueblogs.common.lang.Result;
+import com.klins.oc.blog.vueblogs.entity.User;
 import com.klins.oc.blog.vueblogs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +26,8 @@ public class UserController {
 
     @RequestMapping("index")
     public Object index(){
-        return userService.getById(1L);
+        User user = userService.getById(1L);
+        return Result.succ(user);
     }
 
 }
